cb73a675ac9aa5b053be908beb4310e90cfb43ae
tushar kutre <tusharkutre21@gmail.com>
2025-08-29 11:46:55 +0530

    Added original thumbnail to each video

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,4 +1,6 @@
-import thumbnail from '../assets/thumbnail.png';
+// import thumbnail from '../assets/thumbnail.png';
+import thumbnail1 from '../assets/thumbnail1.jpg';
+import thumbnail2 from '../assets/thumbnail2.jpg';
 
 // array of video objects
 const videos = [
@@ -7,14 +9,14 @@ const videos = [
         title: "Video 1",
         description: "Build a React Image Slider with Tailwind CSS",
         file: "https://ik.imagekit.io/328jzey4c/videos/testVideo.mp4",
-        thumbnail : thumbnail
+        thumbnail : thumbnail1
     },
     {
         id: 2,
         title: "Video 2",
         description: "Top 10 RESUME Projects To ACTUALLY Get Hired In 2025 | Only Backend 🔥",
         file: "https://ik.imagekit.io/328jzey4c/videos/testVideo2.mp4?updatedAt=1756187407934",
-        thumbnail : thumbnail
+        thumbnail : thumbnail2
     },
 ]
 
